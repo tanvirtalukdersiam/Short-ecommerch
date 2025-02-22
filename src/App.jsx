93151452c8ea78d89f2aa9e4a0359cee4f7a8c92
tanvirtalukdersiam <149420877+tanvirtalukdersiam@users.noreply.github.com>
2025-02-22 21:32:25 +0600

    all-done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const App = () => {
+  return (
+    <div>
+      <motion.div className="box-1" drag></motion.div>
+    </div>
+  );
+};
+
+export default App;
